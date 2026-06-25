2361a91ccf6d056cc9bd57593696f3616386e588
Nikola <listotoegey@gmail.com>
2026-06-25 11:54:09 +0300

    Restore higher quick discounts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
 	const STORAGE_KEY = 'dr-biomaster-shop-calculator-v2';
 	const ALL_CATEGORIES = 'Всички';
 	const categories = [ALL_CATEGORIES, ...new Set(products.map((product) => product.category))];
-	const quickDiscounts = [3, 5, 10];
+	const quickDiscounts = [3, 5, 10, 15, 20];
 	const packageComboRules: PackageComboRule[] = [
 		{ packageId: '24886', itemIds: ['259', '1847', '1849'] },
 		{ packageId: '2435', itemIds: ['1865', '1863', '1910'] },
